670f5902a7d62303f0c39f6caf58a2f00bf80685
A Subhash <94886211+AVNSubhash@users.noreply.github.com>
2021-12-14 11:10:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var a=prompt("Welcome To Bhagat Singh! If you want to watch a short video on Bhagat Singh, Type Yes. If No, type No.");
+  	if(a== "Yes")
+  	{
+  	    window.location.href="vid.mp4";
+  	}
+    if(a== "No")
+  	{
+  	    window.location.href="content.html";
+  	}
